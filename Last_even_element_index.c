d808ca024027cea2a23a37335f22eda06714301b
PrachyuthAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-04-26 17:36:14 +0530

    #include<stdio.h> int main() {     int i,n,a[100],A;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             A=i;         }     }     printf("%d",A); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[100],A;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            A=i;
+        }
+    }
+    printf("%d",A);
+}

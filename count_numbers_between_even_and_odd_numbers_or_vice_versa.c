6e26a2d8a3998b71671aa2a67a708ebbf0bdc809
PrachyuthAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-04-26 18:19:41 +0530

    #include<stdio.h> int main() {     int i,n,a[100],A;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n-1;i++)     {         if((a[i+1]%2==0 && a[i-1]%2!=0))         {             A++;         }     }     for(i=0;i<n-1;i++)     {         if((a[i-1]%2==0&&a[i+1]%2!=0))         {             A++;         }     }     printf("%d",A); }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[100],A;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if((a[i+1]%2==0 && a[i-1]%2!=0))
+        {
+            A++;
+        }
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if((a[i-1]%2==0&&a[i+1]%2!=0))
+        {
+            A++;
+        }
+    }   
+    printf("%d",A);
+}

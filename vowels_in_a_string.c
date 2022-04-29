4f1e16b0c386cddad3985ec9193b0eb0c35de414
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-04-29 11:18:33 +0530

    #include<stdio.h> int main() {     int i,x=0;     char str[100],c;     scanf("%[^ ]s",str);     scanf(" %c",&c);     for(i=0;str[i]!=0;i++)     {         if(str[i]==c)         {              printf("True ");             printf("%d ",i);             x++;             break;         }     }     if(x==0)     {         printf("False");     }
    }

---
+++ vowels_in_a_string.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,x=0;
+    char str[100],c;
+    scanf("%[^
+]s",str);
+    scanf(" %c",&c);
+    for(i=0;str[i]!=0;i++)
+    {
+        if(str[i]==c)
+        {
+             printf("True
+");
+            printf("%d
+",i);
+            x++;
+            break;
+        }
+    }
+    if(x==0)
+    {
+        printf("False");
+    }
+    
+}

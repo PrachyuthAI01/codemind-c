a325028a63fffe0c0af338032d5f8aafca116ca7
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-04-30 09:57:46 +0530

    #include<stdio.h> int main() {     int i,c=0;     char str[100];     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         c++;     }     printf("%d",c);
    }

---
+++ characters_in_a_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,c=0;
+    char str[100];
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        c++;
+    }
+    printf("%d",c);
+    
+}

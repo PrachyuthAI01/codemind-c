3e837f7fc2bad4eb66eed4d80adcada588340881
PRACHYUTHAI01 <97424723+PrachyuthAI01@users.noreply.github.com>
2022-06-29 11:30:43 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,r,k=0,v=0,l=0;     scanf("%d",&n);     k=n;     while(n)     {         r=n%10;         l++;         n=n/10;     }     n=k;     while(n)     {         r=n%10;         v=v+pow(r,l);         n=n/10;         l--;     }     if(v==k)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,37 +1,31 @@
 #include<stdio.h>
 #include<math.h>
-int calculateLength(int n)
-{    
-    int length = 0;    
-    while(n != 0)
-	{    
-    	length = length + 1;    
-        n = n/10;    
-    }    
-    return length;    
-}    
-     
-int main()    
-{    
-    int num,sum=0,rem=0,n; 
-	scanf("%d",&num);   
-    int len=calculateLength(num);
-    n = num;   
-    while(num>0)
-	{    
-        rem = num%10;    
-        sum = sum + (int)pow(rem,len);    
-        num = num/10;    
-        len--;    
-    }      
-    if(sum == n)
-	{    
-        printf("True");    
-	}
+int main()
+{
+    int n,r,k=0,v=0,l=0;
+    scanf("%d",&n);
+    k=n;
+    while(n)
+    {
+        r=n%10;
+        l++;
+        n=n/10;
+    }
+    n=k;
+    while(n)
+    {
+        r=n%10;
+        v=v+pow(r,l);
+        n=n/10;
+        l--;
+    }
+    if(v==k)
+    {
+        printf("True");
+    }
     else
-	{
-    
-        printf("False");    
-	}
-    return 0;    
-}    
+    {
+        printf("False");
+    }
+    return 0;
+}
